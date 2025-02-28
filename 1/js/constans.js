80217a31dc9db0826ccb405cdcab7ada7df08181
DialogueWyLToR <patrik.bekesi@dialoguecreatives.com>
2025-02-28 12:01:44 +0100

    base page index.html, with assets

--- 1/js/constans.js
+++ 1/js/constans.js
@@ -1,1 +1,5 @@
 export const code = "NLYG87";
+export const headerHTML = document.querySelector("header");
+export const mainHTML = document.querySelector("main");
+export const asideHTML = document.querySelector("aside");
+export const footerHTML = document.querySelector("footer");
